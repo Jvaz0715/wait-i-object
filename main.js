671f97a311cb8409a03d253a8093e888fc1b3892
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-09 14:25:10 -0500

    Completes assignment

--- main.js
+++ main.js
@@ -85,5 +85,16 @@ console.log(jamesVaz)
 // Part 3 - setting key-value pairs in-line.
 // Now make a new object, but this time we'll pre-populate it, not set it in any lines below it. In other words, the properties should be set the same way I initially set the `codeImmersivesObject` on line 1. It can be called whatever you want and can have whatever properties you want, as long as they include at least one string value, boolean value, number value, and array of numbers. The important thing here is: this should all be done within the brackets of the object, not below the place where the object is declared.
 
+const codingExperience = {
+  fullName: 'James Vazquez',
+  hasCodedPrior: true,
+  monthsCoding: 4,
+  grades: [85, 95, 100, 100],
+};
+
+console.log(codingExperience)
 
 // As a stretch goal, go back pre-populate the object with at least one property using a key name that CAN'T be a variable name. That would be anything with a space in it, or a dash, or starting with a number.
+codingExperience['Biggest challenge so far'] = 'loops'
+console.log(codingExperience)
+
